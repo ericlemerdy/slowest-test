6ed6b7f35f6670f6ebbf3fa495958ae802538563
Eric Le Merdy <eric.lemerdy@laposte.net>
2012-07-10 23:12:16 +0200

    Now read several tests file in the directory

--- src/test/java/fr/free/lemerdy/eric/SlowestSurefireTest.java
+++ src/test/java/fr/free/lemerdy/eric/SlowestSurefireTest.java
@@ -14,15 +14,18 @@ import com.google.common.io.Resources;
 
 public class SlowestSurefireTest {
 	@Test
-    public void should_read_single_surefire_test_report() throws URISyntaxException
+    public void should_read_several_surefire_test_report() throws URISyntaxException
     {
 		SlowestSurefireReports slowestSurefireReports = new SlowestSurefireReports();
 		List<TestReport> testReports = slowestSurefireReports.slowestTests();
 		
-		assertThat(testReports.size()).isEqualTo(7);
-		assertThat(testReports.get(0).time).isEqualTo(0.014d);
-		assertThat(testReports.get(0).classname).isEqualTo("PlainTest");
-		assertThat(testReports.get(0).name).isEqualTo("should_test_1");
+		assertThat(testReports.size()).isEqualTo(9);
+        assertThat(testReports.get(0).time).isEqualTo(0.014d);
+        assertThat(testReports.get(0).classname).isEqualTo("PlainTest");
+        assertThat(testReports.get(0).name).isEqualTo("should_test_plain_1");
+        assertThat(testReports.get(7).time).isEqualTo(9.382d);
+        assertThat(testReports.get(7).classname).isEqualTo("FullTest");
+        assertThat(testReports.get(7).name).isEqualTo("should_test_full_1");
     }
 	
 	@Test
