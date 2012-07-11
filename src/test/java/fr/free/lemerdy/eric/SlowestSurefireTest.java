587e6418093c2d41f52e44c079fcf1e6c70162d3
Eric Le Merdy <eric.lemerdy@laposte.net>
2012-07-11 08:24:19 +0200

    Simplify test case and cover all test results

--- src/test/java/fr/free/lemerdy/eric/SlowestSurefireTest.java
+++ src/test/java/fr/free/lemerdy/eric/SlowestSurefireTest.java
@@ -3,24 +3,33 @@ package fr.free.lemerdy.eric;
 import static org.fest.assertions.Assertions.assertThat;
 
 import java.net.URISyntaxException;
+import java.util.Iterator;
 import java.util.List;
 
 import org.junit.Test;
 
 public class SlowestSurefireTest {
 	@Test
-    public void should_read_several_surefire_test_report() throws URISyntaxException
+    public void with_several_surefire_test_report_should_order_test_results_by_time() throws URISyntaxException
     {
 		SlowestSurefireReports slowestSurefireReports = new SlowestSurefireReports();
 		List<TestReport> testReports = slowestSurefireReports.slowestTests();
 		
-		assertThat(testReports.size()).isEqualTo(9);
-        assertThat(testReports.get(0).time).isEqualTo(0.014d);
-        assertThat(testReports.get(0).classname).isEqualTo("PlainTest");
-        assertThat(testReports.get(0).name).isEqualTo("should_test_plain_1");
-        assertThat(testReports.get(7).time).isEqualTo(9.382d);
-        assertThat(testReports.get(7).classname).isEqualTo("FullTest");
-        assertThat(testReports.get(7).name).isEqualTo("should_test_full_1");
+		assertThat(testReports.size()).isEqualTo(3);
+		Iterator<TestReport> testReportsIterator = testReports.iterator();
+		TestReport testReport = testReportsIterator.next();
+        assertThat(testReport.time).isEqualTo(10.01d);
+        assertThat(testReport.classname).isEqualTo("PlainTest");
+        assertThat(testReport.name).isEqualTo("should_test_plain_1");
+        testReport = testReportsIterator.next();
+        assertThat(testReport.time).isEqualTo(1.001d);
+        assertThat(testReport.classname).isEqualTo("PlainTest");
+        assertThat(testReport.name).isEqualTo("should_test_plain_2");
+        testReport = testReportsIterator.next();
+        assertThat(testReport.time).isEqualTo(5.001d);
+        assertThat(testReport.classname).isEqualTo("FullTest");
+        assertThat(testReport.name).isEqualTo("should_test_full_1");
+        assertThat(testReportsIterator.hasNext()).isFalse();
     }
 	
 }
